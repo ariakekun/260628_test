cb15c2bc7dc99c3809c0be2bf682f2ae77ab42d9
ariakekun <m.piano20201201@gmail.com>
2026-07-04 22:55:56 +0900

    record0704

---
+++ record.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import pandas as pd
+from pykakasi import kakasi
+from supabase import create_client
+
+url = "https://jstwwpwzysrsipfclzuk.supabase.co"
+key = "sb_publishable_PccKZwyxCAPCfGw12rJMoA_JqJ26JDh"
+
+supabase = create_client(url, key)
+
+
+# 調整さんアップロード
+st.write("調整さんからcsvファイルをダウンロードし、それをアップロードしてください。")
+uploaded_file = st.file_uploader("CSV", type="csv")
+
+if uploaded_file:
+    df = pd.read_csv(
+        uploaded_file,
+        header=2
+    )
+
+    names = list(df.columns[1:])  # 「日程」を除く
+    kks = kakasi()
+
+    # リストをあいうえお順に並び替え
+    names = sorted(
+        names,
+        key=lambda x: kks.convert(x)[0]["hira"]
+    )
+
+    # リストに「その他」を追加
+    names.append("その他")
+
+    # 入力させる
+    maisulist = list(i for i in range(1,26))
+
+    person1 = st.selectbox("勝った人", names)
+    if person1 == "その他":
+        person1 = st.text_input("名前を入力してください", key="name1")
+    person2 = st.selectbox("負けた人", names)
+    if person2 == "その他":
+        person2 = st.text_input("名前を入力してください", key="name2")
+    maisu = st.selectbox("枚数差", maisulist)
+    match_date = st.date_input("試合日")
+
+    # 登録
+    if st.button("記録"):
+        if person1 == person2:
+            st.write("登録できません。")
+        else:
+            supabase.table("match_record").insert({
+                "date":match_date.isoformat(),
+                "winner":person1,
+                "loser":person2,
+                "score(maisusa)":maisu
+            }).execute()
+            st.write("正常に記録しました。")
+
